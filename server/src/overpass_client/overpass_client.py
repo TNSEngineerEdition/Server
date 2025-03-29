e7cdf5a1453fd2a4ccb4863a9912156b15ddc344
Aleksander Buszek <133276461+olobuszolo@users.noreply.github.com>
2025-03-29 10:22:17 +0100

    Applying Rafal's suggestions from code review
    Co-authored-by: RCRalph <36704328+RCRalph@users.noreply.github.com>

--- server/src/overpass_client/overpass_client.py
+++ server/src/overpass_client/overpass_client.py
@@ -25,36 +25,16 @@ class OverpassClient:
     out geom;
     """
 
-    _GET_TRAM_STOPS_AND_RAILWAYS_TEMPLATE = """
+    _TRAM_STOPS_AND_TRACKS_TEMPLATE = """
     [out:json];
     area["name"="{area_name}"]->.search_area;
     (
         way["railway"="tram"](area.search_area);
         node["railway"="tram_stop"]["public_transport"="stop_position"](area.search_area);
     );
+    (._; >;);
     out geom;
     """
-
-    _GET_TRAM_STOPs_AND_RAILWAYS_COORDINATE = """
-    [out:json];
-    area["name"="{area_name}"]->.search_area;
-    way["railway"="tram"](area.search_area)->.tram_ways;
-    node(w.tram_ways);
-    out geom;
-    """
-
-    _GET_TRACK_GEOMETRY = """
-    [out:json];
-    area["name"="{area_name}"]->.search_area;
-    (
-    way["railway"="tram"](area.search_area);
-    relation["railway"="tram"](area.search_area);
-    );
-    out body;
-    >;
-    out skel qt;
-    """
-
     @classmethod
     def get_relations_and_stops(cls, area_name: str, custom_node_ids: list[int]):
         if custom_node_ids:
@@ -68,16 +48,6 @@ class OverpassClient:
         return cls._OVERPASS.query(query)
 
     @classmethod
-    def get_tram_stops_and_crossroads(cls, area_name: str):
-        query = cls._GET_TRAM_STOPS_AND_RAILWAYS_TEMPLATE.format(area_name=area_name)
-        return cls._OVERPASS.query(query)
-
-    @classmethod
-    def get_tram_stops_and_crossroads_coordinate(cls, area_name: str):
-        query = cls._GET_TRAM_STOPs_AND_RAILWAYS_COORDINATE.format(area_name=area_name)
-        return cls._OVERPASS.query(query)
-
-    @classmethod
-    def get_track_geometry(cls, area_name: str):
-        query = cls._GET_TRACK_GEOMETRY.format(area_name=area_name)
+    def get_tram_stops_and_tracks(cls, area_name: str):
+        query = cls._TRAM_STOPS_AND_TRACKS_TEMPLATE.format(area_name=area_name)
         return cls._OVERPASS.query(query)
