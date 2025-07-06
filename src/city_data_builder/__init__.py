b967c2fef494370cf33a3624e4814704c9a2592b
RCRalph <rcralph@rcralph.me>
2025-07-06 19:46:04 +0200

    Adjustments for new way of storing city configuration files

--- src/city_data_builder/__init__.py
+++ src/city_data_builder/__init__.py
@@ -1,4 +1,11 @@
 from .city_configuration import CityConfiguration
 from .city_data_builder import CityDataBuilder
+from .model import ResponseGraphNode, ResponseGraphTramStop, ResponseTramTrip
 
-__all__ = ["CityConfiguration", "CityDataBuilder"]
+__all__ = [
+    "CityConfiguration",
+    "CityDataBuilder",
+    "ResponseGraphNode",
+    "ResponseGraphTramStop",
+    "ResponseTramTrip",
+]
