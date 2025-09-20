25d372fcaf6552bf79ad9d6dad87cc89a5d7b0b9
olobuszolo <olekbuszek@gmail.com>
2025-09-20 18:41:37 +0200

    mock for response city data

--- tests/test_city_cache_data.py
+++ tests/test_city_cache_data.py
@@ -1,20 +1,77 @@
 import datetime
+import shutil
+import tempfile
 from pathlib import Path
+from typing import Any, Generator
+
+import pytest
 
 from city_data_builder import (
     ResponseCityData,
+    ResponseGraphEdge,
+    ResponseGraphNode,
+    ResponseGraphTramStop,
+    ResponseTramRoute,
+    ResponseTramTrip,
 )
 from city_data_cache import CityDataCache
 
 
 class TestCityDataCache:
+    @pytest.fixture(scope="class")
+    def response_city_data_mock(self) -> ResponseCityData:
+        tram_graph = [
+            ResponseGraphNode(
+                id=1,
+                lat=50.0,
+                lon=19.0,
+                neighbors={
+                    2: ResponseGraphEdge(
+                        id=2, distance=100.0, azimuth=90.0, max_speed=5.0
+                    )
+                },
+            ),
+            ResponseGraphTramStop(
+                id=2,
+                lat=50.0005,
+                lon=19.0005,
+                name="Centrum",
+                gtfs_stop_ids=["centrum_1"],
+                neighbors={},
+            ),
+        ]
+
+        tram_routes = [
+            ResponseTramRoute(
+                name="1",
+                background_color="",
+                text_color="",
+                trips=[
+                    ResponseTramTrip(
+                        trip_head_sign="Centrum",
+                        stops=[],
+                    )
+                ],
+            )
+        ]
+
+        return ResponseCityData(tram_track_graph=tram_graph, tram_routes=tram_routes)
+
+    @pytest.fixture(scope="class")
+    def cache_directory(
+        self, response_city_data_mock: ResponseCityData
+    ) -> Generator[Path, Any, None]:
+        directory_path = Path(tempfile.mkdtemp())
+        cache = CityDataCache(directory_path)
+        cache.store("krakow", datetime.date(2025, 1, 1), response_city_data_mock)
+        yield directory_path
+        shutil.rmtree(directory_path)
+
     def test_get_data(
-        self,
-        city_cache_dir: Path,
-        krakow_response_city_data: ResponseCityData,
+        self, cache_directory: Path, response_city_data_mock: ResponseCityData
     ) -> None:
         # Arrange
-        cache = CityDataCache(city_cache_dir)
+        cache = CityDataCache(cache_directory)
         city_id, date = "krakow", datetime.date(2025, 1, 1)
 
         # Act
@@ -22,7 +79,7 @@ class TestCityDataCache:
 
         # Assert
         assert isinstance(loaded_data, ResponseCityData)
-        assert loaded_data == krakow_response_city_data
+        assert loaded_data == response_city_data_mock
 
     def test_store_data(
         self, city_cache_dir: Path, krakow_response_city_data: ResponseCityData
