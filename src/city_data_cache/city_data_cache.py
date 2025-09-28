4de4fc1510225a2531bb5c81d5af25b7a5a882ef
olobuszolo <olekbuszek@gmail.com>
2025-09-28 17:27:31 +0200

    openinng zip in helper func only once

--- src/city_data_cache/city_data_cache.py
+++ src/city_data_cache/city_data_cache.py
@@ -53,19 +53,19 @@ class CityDataCache:
                 reverse=True,
             )
 
-    def _rebuild_zip(self, city_zip_path: Path, files_to_copy: list[str]) -> None:
+    def _rebuild_zip(self, zip_file: ZipFile, files_to_copy: list[str]) -> Path:
         copy_set = set(files_to_copy)
         with tempfile.NamedTemporaryFile("wb", delete=False) as temp_file:
             tmp_path = Path(temp_file.name)
 
-        with ZipFile(city_zip_path) as zip_read:
-            with ZipFile(tmp_path, "w") as zip_write:
-                for item in zip_read.infolist():
-                    if item.filename in copy_set:
-                        with zip_read.open(item.filename) as file_to_copy:
-                            data = file_to_copy.read()
-                        zip_write.writestr(item, data)
-        shutil.move(str(tmp_path), str(city_zip_path))
+        with ZipFile(tmp_path, "w") as zip_write:
+            for item in zip_file.infolist():
+                if item.filename in copy_set:
+                    with zip_file.open(item.filename) as file_to_copy:
+                        data = file_to_copy.read()
+                    zip_write.writestr(item, data)
+
+        return tmp_path
 
     def _remove_redundant_files(self, city_zip_path: Path) -> None:
         file_count = len(ZipFile(city_zip_path, "r").namelist())
@@ -73,9 +73,11 @@ class CityDataCache:
             return
         with ZipFile(city_zip_path) as zip_file:
             files = [name for name in zip_file.namelist()]
-        files.sort(reverse=True)
-        files_to_copy = files[: self.max_file_count - 1]
-        self._rebuild_zip(city_zip_path, files_to_copy)
+            files.sort(reverse=True)
+            files_to_copy = files[: self.max_file_count - 1]
+            tmp_path = self._rebuild_zip(zip_file, files_to_copy)
+
+        shutil.move(str(tmp_path), str(city_zip_path))
 
     def store(
         self,
@@ -92,7 +94,7 @@ class CityDataCache:
 
         self._remove_redundant_files(city_zip_path)
 
-        with ZipFile(city_zip_path, "a") as zip_file:
+        with ZipFile(city_zip_path, "a", compression=ZIP_DEFLATED) as zip_file:
             zip_file.writestr(file_name, city_data.model_dump_json())
 
         return city_data
